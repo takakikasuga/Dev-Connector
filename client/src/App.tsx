65bcac7680ba896060a882ed6fec53fd0933f1ed
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-14 07:10:34 +0900

    Reduxストアの作成

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,21 +5,26 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App: FC = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing}></Route>
-        <section className='container'>
-          <Switch>
-            <Route exact path='/register' component={Register}></Route>
-            <Route exact path='/login' component={Login}></Route>
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing}></Route>
+          <section className='container'>
+            <Switch>
+              <Route exact path='/register' component={Register}></Route>
+              <Route exact path='/login' component={Login}></Route>
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 export default App;
