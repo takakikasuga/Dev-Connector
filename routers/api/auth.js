a5c4b8f1b5b7f545b23e77c933650dca9f9d9ada
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-13 17:32:20 +0900

    ユーザー認証/ログインルート

--- routers/api/auth.js
+++ routers/api/auth.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { body, validationResult } = require('express-validator');
 
 // @route     GET api/auth
 // @desc      Test route
 // @access    Public
+
+// auth：ヘッダーに有効なトークンを確認したら（ログインしている状態でGetリクエスト）
 router.get('/', auth, async (req, res) => {
   try {
     //.select('-password');は返り値にパスワードを含めないようにすること。
@@ -16,5 +22,71 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route     POST api/suth
+// @desc       Authenticate user & get token
+// @access    Public
+router.post(
+  '/',
+  [
+    body('email', 'Please include a valid email address').isEmail(),
+    body('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    // エラーがあるか否かを確認する
+    const errors = validationResult(req);
+    console.log('errors', errors);
+    console.log('errors.isEmpty()', errors.isEmpty());
+    console.log(req.body);
+    if (!errors.isEmpty())
+      //  errors.array();でエラーの内容をレスポンスする
+      return res.status(400).json({ errors: errors.array() });
+
+    const { email, password } = req.body;
+    try {
+      // 同じユーザーが既に存在するか否かの確認（既に存在したら登録できないようにする）
+      let user = await User.findOne({ email });
+      // ユーザーがいない場合はエラーを出す
+      if (!user) {
+        return res
+          .status(404)
+          .json({ errors: [{ msg: ' Invalid Credentials' }] });
+      }
+
+      // 普通のパスワードと暗号化したパスワードが一致しているかを確認する
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(404)
+          .json({ errors: [{ msg: ' Invalid Credentials' }] });
+      }
+
+      console.log('user', user);
+      // jwtを返す
+      const payload = {
+        user: {
+          // _idとしなくてもidでできるようになっている
+          id: user._id
+        }
+      };
+
+      console.log('payload', payload);
+      // 3600;は1時間・jwt.signはトークンを発行する
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
--- routers/api/users.js
+++ routers/api/users.js
@@ -1,11 +1,96 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { body, validationResult } = require('express-validator');
+const User = require('../../models/User');
 
-// @route     GET api/users
-// @desc      Test route
+// @route     POST api/users
+// @desc      Register User
 // @access    Public
-router.get('/', (req, res) => {
-  res.send('User Router');
-});
+router.post(
+  '/',
+  [
+    // 名前が渡ってきているかどうか（空ではない）
+    body('name', 'Name is required').not().isEmpty(),
+    // password must be at least 5 chars long
+    body('email', 'Please include a valid email address').isEmail(),
+    body(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log('errors', errors);
+    console.log('errors.isEmpty()', errors.isEmpty());
+    console.log(req.body);
+    if (!errors.isEmpty())
+      //  errors.array();でエラーの内容をレスポンスする
+      return res.status(400).json({ errors: errors.array() });
+
+    const { name, email, password } = req.body;
+    try {
+      // 同じユーザーが既に存在するか否かの確認（既に存在したら登録できないようにする）
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(404)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // ユーザーのアバターを取得する（gravatarモジュール）
+      // s：文字列（String）、r：評価（Rating）、d：標準（ Default）
+      const avatar = gravatar.url(email, {
+        size: '200',
+        rating: 'pg',
+        // mmはデフォルトで画像を差し込んでくれる（ミステリーマン）
+        default: 'mm'
+      });
+
+      // 保存するためのインスタンスを作成
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // パスワードのハッシュ化
+      // bcrypt.getSalt(10)の10はドキュメントの推奨
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // DBに保存
+      await user.save();
+
+      console.log('user', user);
+      // jwtを返す
+      const payload = {
+        user: {
+          // _idとしなくてもidでできるようになっている
+          id: user._id
+        }
+      };
+
+      console.log('payload', payload);
+      // 3600;は1時間・jwt.signはトークンを発行する
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
