7f8ecd6ef20e714ac0bb27afb7b68f0eda33b131
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-13 16:24:57 +0900

    11. User Registration

--- routers/api/posts.js
+++ routers/api/posts.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
+const User = require('../../models/User');
 
 // @route     POST api/posts
 // @desc      Register User
@@ -17,7 +20,7 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     console.log('errors', errors);
     console.log('errors.isEmpty()', errors.isEmpty());
@@ -25,7 +28,45 @@ router.post(
     if (!errors.isEmpty())
       //  errors.array();でエラーの内容をレスポンスする
       return res.status(400).json({ errors: errors.array() });
-    res.send('Posts Router');
+
+    const { name, email, password } = req.body;
+    try {
+      // 同じユーザーが既に存在するか否かの確認（既に存在したら登録できないようにする）
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(404)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // ユーザーのアバターを取得する（gravatarモジュール）
+      // s：文字列（String）、r：評価（Rating）、d：標準（ Default）
+      const avatar = gravatar.url(email, {
+        size: '200',
+        rating: 'pg',
+        // mmはデフォルトで画像を差し込んでくれる（ミステリーマン）
+        default: 'mm'
+      });
+
+      // 保存するためのインスタンスを作成
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // パスワードのハッシュ化
+      // bcrypt.getSalt(10)の10はドキュメントの推奨
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User register');
+    } catch (err) {}
+    console.error(err.message);
+    res.status(500).send('Server error');
   }
 );
 
