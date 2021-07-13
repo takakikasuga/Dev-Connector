1f62f25235b1e3176d6f93f6841b913580bf532e
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-13 17:04:17 +0900

    カスタム認証ミドルウェアとJWT検証

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+  // ヘッダーからトークンを取得する（x-auth-token：キー名称）
+  const token = req.header('x-auth-token');
+
+  // トークンの有無を確認
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  try {
+    // jwtトークンが今でも有効化を確認する（返り値はペイロード）
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    console.log('decoded', decoded);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({
+      msg: 'Token is not valid'
+    });
+  }
+};
