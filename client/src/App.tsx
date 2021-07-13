012d4daeb2cd8190d149e4a70b68bfada0d7a339
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-14 06:29:48 +0900

    Reactルーターのセットアップ

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,25 @@
 import React, { FC, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App: FC = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing}></Route>
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register}></Route>
+            <Route exact path='/login' component={Login}></Route>
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
   );
 };
 export default App;
