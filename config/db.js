fde5a33413bf075705b8fa0c836cf8c27ea0b77b
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-13 12:47:17 +0900

    Mongooseを使用したMongoDBへの接続

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+console.log('db', db);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+
+    // 接続に失敗した場合にアプリを強制終了させる
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
