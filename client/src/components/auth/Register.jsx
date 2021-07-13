3fa090f5e684b2c88f218b9d8a52c7831e1b5bd0
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-14 06:56:28 +0900

    リクエスト例とログインフォーム

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Register = () => {
   const [formData, setFormData] = useState({
