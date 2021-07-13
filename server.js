4585c5fcd30782388343fa17f783abb77401eaea
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-13 13:01:44 +0900

    ExpressRouterを使用してファイルをルーティングする

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const connectDB = require('./config/db');
+// ルートの読み込み
+const usersRouter = require('./routers/api/users');
+const authRouter = require('./routers/api/auth');
+const profileRouter = require('./routers/api/profile');
+const postsRouter = require('./routers/api/posts');
 
 const app = express();
 
@@ -9,6 +14,12 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('API Running');
 });
+
+// 各種のルート定義
+app.use('/api/users', usersRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/profile', profileRouter);
+app.use('/api/posts', postsRouter);
 
 const PORT = process.env.PORT || 5000;
 
