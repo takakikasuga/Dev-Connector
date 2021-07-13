5174309b3e00338330f1324ff9638aed0d4003a5
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-13 18:45:35 +0900

    Create & Update Profile Routes

--- routers/api/profile.js
+++ routers/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { body, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -29,5 +30,92 @@ router.get('/me', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route     POST api/profile
+// @desc      Create or update user profile
+// @access    Private
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      body('status', 'Status is required').not().isEmpty(),
+      body('skills', 'Skills is required').not().isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // destructure the request
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook
+      // spread the rest of the fields we don't need to check
+      // ...rest
+    } = req.body;
+
+    // プロフィールオブジェクトの初期化
+    const profileFields = {};
+    // req.user.id;はユーザーの一意のID（ミドルウェアで取得）
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(',').map((skill) => skill.trim());
+    }
+
+    console.log('skills', skills);
+    console.log('profileFields', profileFields);
+
+    // ソーシャルネットワークオブジェクトの初期化
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (facebook) profileFields.social.facebook = facebook;
+
+    console.log('profileFields', profileFields);
+
+    try {
+      //  req.user.id;はユーザーの一意のID
+      let profile = await Profile.findOne({ user: req.user.id });
+      if (profile) {
+        // Update（既にプロフィール情報があった場合は更新をする）
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+
+      //Create
+      profile = new Profile(profileFields);
+      await profile.save();
+      return res.json(profile);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
