4b221b3577e0954c70d626420a7a0d5e92063ae8
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-14 14:40:03 +0900

    Load User & Set Auth Token

---
+++ client/src/App.jsx
@@ -0,0 +1,40 @@
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
+
+if (localStorage.token) {
+  console.log('App.tsxのlocalStorage.token');
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadUser());
+  }, []);
+
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing}></Route>
+        <section className='container'>
+          <Alert />
+          <Switch>
+            <Route exact path='/register' component={Register}></Route>
+            <Route exact path='/login' component={Login}></Route>
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  );
+};
+export default App;
